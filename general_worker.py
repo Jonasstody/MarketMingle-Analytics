958c91ec6246620ebeeb3631adf65da73b68c720
ymsah <72617299+Jonasstody@users.noreply.github.com>
2023-12-05 13:47:33 +0000

    direct2api_search

--- general_worker.py
+++ general_worker.py
@@ -1,13 +1,17 @@
 import yahooquery as yq
+import json
+import urllib
 
 def check_if_public(search_input):
-    search = yq.search(search_input)
     try:
-        ticker_symbol = search['quotes'][0]['symbol']
-        long_name  = search['quotes'][0]['longname']
-        return True, False, ticker_symbol, long_name
+        response = urllib.request.urlopen(f'https://query2.finance.yahoo.com/v1/finance/search?q={search_input}')
+        content = response.read()
+        request = json.loads(content.decode('utf8'))['quotes'][0]
+        name = request['shortname']
+        symbol = request['symbol']
+        return True, False, symbol, name
     except:
         return False, True, 'n/a', 'n/a'
-    
+
 def check_if_private(search_input):
     return False
