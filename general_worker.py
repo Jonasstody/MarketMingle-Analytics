c81fdb5e3c609a682c92798bf4280cc1feec5692
ymsah <72617299+Jonasstody@users.noreply.github.com>
2023-12-08 16:14:18 +0000

    update search

--- general_worker.py
+++ general_worker.py
@@ -1,4 +1,5 @@
 import yahooquery as yq
+from time import sleep
 
 def check_if_public(search_input):
     search = yq.search(search_input)
@@ -7,7 +8,14 @@ def check_if_public(search_input):
         long_name  = search['quotes'][0]['longname']
         return True, False, ticker_symbol, long_name
     except:
-        return False, True, 'n/a', 'n/a'
+        sleep(0.5)
+        try:
+            search = yq.search(search_input)
+            ticker_symbol = search['quotes'][0]['symbol']
+            long_name  = search['quotes'][0]['longname']
+            return True, False, ticker_symbol, long_name
+        except:
+            return False, True, 'n/a', 'n/a'
 
 def check_if_private(search_input):
     return False
